3de061a2e69c044117b518f162c715c0caa2b703
zgildo01 <zzaragozagildo@gmail.com>
2022-10-03 08:20:46 -0400

    add show profile functionality

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,6 +5,7 @@ import * as profilesCtrl from '../controllers/profiles.js'
 const router = Router()
 
 router.get('/', profilesCtrl.index)
+router.get('/:id', profilesCtrl.show)
 
 export {
   router
